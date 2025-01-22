c0751f957d3e41906e58990d67d67680ddd422a3
AriadnaVelazquez744 <velazquezreyariadna@gmail.com>
2025-01-21 19:44:09 -0500

    ajustes en los formularios relacionados a los ultimos cambios de los campos de la BD

--- Baseball_Management/src/components/FormulariosCRUD/WorkerCRUD.jsx
+++ Baseball_Management/src/components/FormulariosCRUD/WorkerCRUD.jsx
@@ -5,12 +5,12 @@ import BaseCRUD from "./BaseCRUD";
 
 const WorkerCRUD = () => {
     const fields = [
-        { name: "CI", label: "CI", type: "number", nullable: false },
-        { name: "DT_id", label: "DT ID", type: "number", nullable: true },
+        { name: "P_id", label: "Person ID", type: "number", nullable: false },
+        { name: "DT_id", label: "Director Técnico ID", type: "number", nullable: true },
     ];
     const apiUrl = "http://127.0.0.1:8000/workers/";
     const initialFormValues = {
-        CI: "",
+        P_id: "",
         DT_id: "",
     };
 
