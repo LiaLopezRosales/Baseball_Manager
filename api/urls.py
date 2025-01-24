697b2d88629117fe82d08183123abfa788e363e3
gaboCiber <gabopla.lasa@gmail.com>
2025-01-24 17:08:22 -0500

    .

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 # api/urls.py
 
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LoginView, StatisticsView, TeamLineupView, AdminDashboardView, CambioViewSet
+
 
 # Definición del router para los ViewSets (CRUD automático)
 router = DefaultRouter()
@@ -14,5 +15,6 @@ urlpatterns = [
     path('stats/', StatisticsView.as_view(), name='statistics'),
     path('team/<int:team_id>/lineup/', TeamLineupView.as_view(), name='team_lineup'),
     path('admin-dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),
+    path('queries/', include('api.reports.urls')),
 ] + router.urls  # Agrega automáticamente las rutas CRUD para el ViewSet
 
