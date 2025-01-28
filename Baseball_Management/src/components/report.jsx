39cb058be18162fce60806bb4ff83e5662ce122d
CarlosDLargacha <lamboveneno4.0@gmail.com>
2025-01-28 18:58:55 -0500

    Funcionalidad de exportación de reportes añadida

--- Baseball_Management/src/components/report.jsx
+++ Baseball_Management/src/components/report.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 function ParamsSelector({ report_id, onTeamSelect, onPitcherSelect, onPitcherLNSelect, onSeasonSelect, onSeriesSelect }) {
-
     const [data, setData] = useState(null);
     const [data2, setData2] = useState(null);
     const [data3, setData3] = useState(null);
@@ -18,27 +17,27 @@ function ParamsSelector({ report_id, onTeamSelect, onPitcherSelect, onPitcherLNS
     // Manejar el cambio de selección
     const handleTeamChange = (event) => {
         setSelectedTeam(event.target.value);
-        onTeamSelect(event.target.value)
+        onTeamSelect(event.target.value);
     };
 
     const handlePitcherChange = (event) => {
         setSelectedPitcherN(event.target.value);
-        onPitcherSelect(event.target.value)
+        onPitcherSelect(event.target.value);
     };
 
     const handlePitcherLastnameChange = (event) => {
         setSelectedPitcherLn(event.target.value);
-        onPitcherLNSelect(event.target.value)
+        onPitcherLNSelect(event.target.value);
     };
 
     const handleSeasonChange = (event) => {
         setSelectedSeason(event.target.value);
-        onSeasonSelect(event.target.value)
+        onSeasonSelect(event.target.value);
     };
 
     const handleSeriesChange = (event) => {
         setSelectedSeries(event.target.value);
-        onSeriesSelect(event.target.value)
+        onSeriesSelect(event.target.value);
     };
 
     useEffect(() => {
@@ -131,47 +130,46 @@ function ParamsSelector({ report_id, onTeamSelect, onPitcherSelect, onPitcherLNS
             return (
                 <select name="season" id="season" value={selectedSeason} onChange={handleSeasonChange}>
                     <option value="">-- Selecciona una temporada --</option>
-                    {data3 && data3.map(option => ( 
-                        <option key={option.id} value={option.name}> 
-                            {option.name} 
-                        </option>
-                    ))}
-                </select>
-            )
-        case 1:
-            return (
-                <select name="series" id="series" value={selectedSeries} onChange={handleSeriesChange}>
-                <option value="">-- Selecciona una serie --</option>
-                    {data4 && data4.map(option => ( 
+                    {data3 && data3.map(option => (
                         <option key={option.id} value={option.name}>
                             {option.name}
                         </option>
                     ))}
                 </select>
-            )
+            );
+        case 1:
+            return (
+                <select name="series" id="series" value={selectedSeries} onChange={handleSeriesChange}>
+                    <option value="">-- Selecciona una serie --</option>
+                    {data4 && data4.map(option => (
+                        <option key={option.id} value={option.name}>
+                            {option.name}
+                        </option>
+                    ))}
+                </select>
+            );
         case 4:
             return (
                 <div>
                     <input type='text' name="pitcher_n" id="pitcher" value={selectedPitcherName} autoCapitalize='words' placeholder='Nombre del pitcher' onChange={handlePitcherChange}></input>
                     <input type='text' name="pitcher_ln" id="pitcher2" value={selectedPitcherLastname} autoCapitalize='words' placeholder='Apellido del pitcher' onChange={handlePitcherLastnameChange}></input>
                 </div>
-            )
+            );
         case 8:
             return (
                 <select name="team" id="team" value={selectedTeam} onChange={handleTeamChange}>
-                <option value="">-- Selecciona un equipo --</option>
-                    {data && data.map(option => ( 
+                    <option value="">-- Selecciona un equipo --</option>
+                    {data && data.map(option => (
                         <option key={option.id} value={option.name}>
                             {option.name}
                         </option>
                     ))}
                 </select>
-            )
+            );
         default:
-            return( <div></div> )
+            return <div></div>;
     }
 }
-
 
 const ReportComponent = ({ report_id, report_name }) => {
     const [data, setData] = useState();
@@ -183,10 +181,11 @@ const ReportComponent = ({ report_id, report_name }) => {
     const [pitcher_lastname, setSelectedPitcherLastname] = useState('');
     const [season_name, setSelectedSeason] = useState('');
     const [serie_name, setSelectedSeries] = useState('');
+    const [exportFormat, setExportFormat] = useState('pdf'); // Estado para el formato de exportación
+    const [exportJson, setExportJson] = useState(null); // Estado para almacenar el JSON que se enviará
 
     useEffect(() => {
-
-        let params
+        let params;
         switch (report_id) {
             case 3:
             case 5:
@@ -197,10 +196,10 @@ const ReportComponent = ({ report_id, report_name }) => {
             case 0:
             case 2:
                 params = new URLSearchParams({ report_id, season_name });
-                break; 
-            case 1:  
+                break;
+            case 1:
                 params = new URLSearchParams({ report_id, serie_name });
-                break; 
+                break;
             case 4:
                 params = new URLSearchParams({ report_id, pitcher_name, pitcher_lastname });
                 break;
@@ -208,9 +207,9 @@ const ReportComponent = ({ report_id, report_name }) => {
                 params = new URLSearchParams({ report_id, team_name });
                 break;
             default:
-                break;      
+                break;
         }
-        
+
         const url = `http://localhost:8000/api/queries/reports/?${params.toString()}`;
 
         // Realizar la solicitud GET
@@ -229,50 +228,104 @@ const ReportComponent = ({ report_id, report_name }) => {
                 setError(error); // Guardar el error en el estado
                 setLoading(false); // Cambiar el estado de carga
             });
-    }, [report_id, season_name, serie_name, pitcher_name, pitcher_lastname, team_name]); // El efecto se ejecuta solo una vez al montar el componente
+    }, [report_id, season_name, serie_name, pitcher_name, pitcher_lastname, team_name]);
+
+    const handleExport = async () => {
+        // Asegúrate de que `data` sea un objeto con la clave "Sección 1"
+        const exportData = {
+            filename: report_name,
+            format: exportFormat,
+            data: {
+                "": Array.isArray(data) ? data : data[""] // Asegúrate de que sea un array
+            }
+        };
+    
+        // Actualiza el estado con el JSON que se enviará
+        setExportJson(exportData);
+    
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/queries/export/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(exportData),
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const blob = await response.blob();
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${report_name}.${exportFormat}`;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+        } catch (error) {
+            console.error('Error exporting report:', error);
+        }
+    };
 
     if (loading) {
-        return <div>Cargando...</div>; // Mensaje mientras se cargan los datos
+        return <div>Cargando...</div>;
     }
 
     if (error) {
-        return <div>Error: {error.message}</div>; // Mensaje de error si ocurre uno
+        return <div>Error: {error.message}</div>;
     }
 
     return (
         <div>
+            <h1>{report_name}</h1>
 
-        <h1>{report_name}</h1>
+            <ParamsSelector
+                report_id={report_id}
+                onTeamSelect={setSelectedTeam}
+                onPitcherSelect={setSelectedPitcher}
+                onPitcherLNSelect={setSelectedPitcherLastname}
+                onSeasonSelect={setSelectedSeason}
+                onSeriesSelect={setSelectedSeries}
+            />
 
-        <ParamsSelector 
-            report_id={report_id}
-            onTeamSelect={setSelectedTeam} 
-            onPitcherSelect={setSelectedPitcher}
-            onPitcherLNSelect={setSelectedPitcherLastname} 
-            onSeasonSelect={setSelectedSeason} 
-            onSeriesSelect={setSelectedSeries}
-        />
+            <div>
+                <select value={exportFormat} onChange={(e) => setExportFormat(e.target.value)}>
+                    <option value="pdf">PDF</option>
+                    <option value="csv">CSV</option>
+                </select>
+                <button onClick={handleExport}>Exportar</button>
+            </div>
 
-        <ul>
-        {Array.isArray(data) && data.length > 0 ? (
-                data.map((item, index) => (
-                    <li key={index}>
-                        <strong>Elemento {index + 1}:</strong>
-                        <ul>
-                            {Object.keys(item).map(key => (
-                                <li key={key}> {key}: {item[key]} </li>
-                            ))}
-                        </ul>
-                    </li>
-                ))
-            ) : (
+            {/* Mostrar el JSON que se enviará }
+            {exportJson && (
                 <div>
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
+                    <h2>JSON que se enviará:</h2>
+                    <pre>{JSON.stringify(exportJson, null, 2)}</pre>
                 </div>
-            )}
-        </ul>
-    </div>
-        );
+            )}*/}
+
+            <ul>
+                {Array.isArray(data) && data.length > 0 ? (
+                    data.map((item, index) => (
+                        <li key={index}>
+                            <strong>Elemento {index + 1}:</strong>
+                            <ul>
+                                {Object.keys(item).map(key => (
+                                    <li key={key}> {key}: {item[key]} </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))
+                ) : (
+                    <div>
+                        <pre>{JSON.stringify(data, null, 2)}</pre>
+                    </div>
+                )}
+            </ul>
+        </div>
+    );
 };
 
 export default ReportComponent;
